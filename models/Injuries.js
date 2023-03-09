e0cc761e6679eef2bea6f0e79115087752b0216f
Louayelaroui <94923176+Louayelaroui@users.noreply.github.com>
2023-03-09 23:09:19 +0100

    Update Injuries.js

--- models/Injuries.js
+++ models/Injuries.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+const commentSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  postedBy: { type: mongoose.Schema.Types.ObjectId, 
+    ref: "Injury" },
+  createdAt: { type: Date, default: Date.now },
+});
 
 const injurySchema = new mongoose.Schema(
   {
@@ -7,7 +13,7 @@ const injurySchema = new mongoose.Schema(
     degree :{type : String,  required: true},
     description :{type : String,  required: true},
     datedebut :{type : String,  required: true}, 
-    comment :[{type : String}],
+    comments: [commentSchema],
     urlimage:{type:String}
   },
   { timestamps: true }

c07a309726ce6ee38316638082de7b4866f12886
Louayelaroui <94923176+Louayelaroui@users.noreply.github.com>
2023-03-09 22:14:52 +0100

    Update Pdf.js

--- models/Pdf.js
+++ models/Pdf.js
@@ -2,11 +2,14 @@ const mongoose = require("mongoose");
 
 const pdfSchema = new mongoose.Schema(
   {
-    
-    pdfurl :{data:Buffer, contentType: String }, 
-   
+    player: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Player",
+      required: true,
+    },
+    pdfUrl: [{ data: Buffer, contentType: String }],
   },
   { timestamps: true }
-);
+)
 
 module.exports = mongoose.model("Pdf", pdfSchema);

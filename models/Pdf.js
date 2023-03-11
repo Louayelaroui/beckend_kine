dba8fb0574b84d00a31149a420f66d08ea09b79b
Louayelaroui <94923176+Louayelaroui@users.noreply.github.com>
2023-03-11 13:53:50 +0100

    Update Pdf.js

--- models/Pdf.js
+++ models/Pdf.js
@@ -1,19 +1,27 @@
 const mongoose = require("mongoose");
 
-const pdfSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    player: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Player",
-      required: true,
-    },
-    pdfUrl: { Type: String },
-  },
-  { timestamps: true }
-);
+const pdfSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  postedBy: { type: mongoose.Schema.Types.ObjectId, 
+    ref: "Player" },
+  pdfUrl: { Type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+}, { timestamps: true });
+
+// const pdfSchema = new mongoose.Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     player: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Player",
+//       required: true,
+//     },
+//     pdfUrl: { Type: String },
+//   },
+//   { timestamps: true }
+// );
 
 module.exports = mongoose.model("Pdf", pdfSchema);
